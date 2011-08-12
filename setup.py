2d063cd5b024762c5746d3e2f46a33baae911002
Gabriel Grant <g@briel.ca>
2011-08-11 18:18:51 -0700

    initial import of setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='django-billing',
+    version='0.1.0dev',
+    author='Gabriel Grant',
+    packages=[
+        'billing',
+        'billing.processor',
+        'billing.processor.simple_account',
+        'billing.tests',
+    ],
+    license='LGPL',
+    long_description=open('README').read(),
+    install_requires=[
+        'python-pricing',
+        'django-annoying',
+        'django-model-utils',
+        'South',
+    ],
+    dependency_links = [
+    	'http://github.com/gabrielgrant/python-pricing/tarball/master#egg=python-pricing',
+    ]
+)
